0a8d866ce99eecf749158f6a90b320a74a7c1ec7
PiotrekGelert <piotrekgelert@gmail.com>
2023-10-30 16:46:38 +0000

    small description of gui

--- image_editor.py
+++ image_editor.py
@@ -2,3 +2,14 @@ import os
 import tkinter as tk
 
 import PIL
+
+'''
+the gui:
+BUTTONS AREA:
+Image file: [open file, open url, convert to: jpeg, png, tiff, others],
+Edit image: [crop, flip, rotate, resize],
+Colors: [color_balance, contrast, COL_to_BW, brightness, sharpness, noise]
+Filters: [blur, contour, edge_enhance, emboss, unsharp, smooth]
+IMAGE AREA:
+SLIDERS AREA: [save to image butt(closes slider), ]
+'''
