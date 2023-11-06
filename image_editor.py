16f2bc826070db0c9b697098c167329f441a0150
PiotrekGelert <piotrekgelert@gmail.com>
2023-11-06 15:38:31 +0000

    open image from recent folders added

--- image_editor.py
+++ image_editor.py
@@ -29,7 +29,19 @@ class MainApp(tk.Tk):
         self.geometry('1080x810')
         self.resizable(False, False)
         self.path_ = []
+        self.recent_paths_saver()
         self.widgets()
+
+    def recent_paths_saver(self):
+        file_comp = 'recent_comp_paths.txt'
+        file_url = 'recent_url_paths.txt'
+        if (file_comp and file_url) not in os.listdir():
+            f_comp = open(file_comp, 'x')
+            f_url = open(file_url, 'x')
+            f_comp.close()
+            f_url.close()
+        else:
+            pass
 
     def buttons_placement_field(self):
         backgr = 'green'
@@ -320,7 +332,7 @@ class OpenImageSelector:
     def __init__(self , update_a, update_b= None):  #
         top = tk.Toplevel()
         top.title('Select method to open the image')
-        top.geometry('400x250')
+        top.geometry('400x400')
         self.update_a = update_a
         self.update_b = update_b
         drives = [f'{chr(x)}' for x in range(61, 91) if os.path.exists(f'{chr(x)}:\\')]
@@ -334,9 +346,9 @@ class OpenImageSelector:
         drives_label.pack()
         drives_label.place(x=20, y=3)
 
-        self.drives_text_field = tk.Entry(top)
-        self.drives_text_field.pack()
-        self.drives_text_field.place(x=20, y=60, height=25, width=360)
+        self.drives_field = tk.Entry(top)
+        self.drives_field.pack()
+        self.drives_field.place(x=20, y=60, height=25, width=360)
 
         drives_button = tk.Button(
             top,
@@ -357,18 +369,50 @@ class OpenImageSelector:
 
         folder_button = tk.Button(
             top,
-            text= 'Submit path to the folder',
+            text= 'Submit path',
             command=lambda: [self.submit_folder(), top.destroy()]  # , self.open_image_path()
         )
         folder_button.pack()
         folder_button.place(x=20, y=190)
-    
+        
+        resent_label = tk.Label(top, text='Paths to the recently opened folders')
+        resent_label.pack()
+        resent_label.place(x=20, y=240)
+
+        self.recent_field = tk.Listbox(top)
+        self.recent_field.pack()
+        self.recent_field.place(x=20, y=260, width=360, height=100)
+
+        recent_button = tk.Button(
+            top,
+            text='Submit selected path',
+            command=lambda:[self.submit_recent(), top.destroy()])
+        recent_button.pack()
+        recent_button.place(x=20, y=365)
+
+        self.recent_ls = []
+        with open('recent_comp_paths.txt', 'r') as f:
+            for recent_file in f.readlines():
+                rc = '\\'.join(recent_file.split('\\')[:-2])
+                self.recent_ls.insert(0, rc)
+                self.recent_field.insert(0, rc)
+
     def submit_folder(self):
-        OpenPathImage(self.update_a, self.folder_field.get())
-        # self.update(self.folder_field)
+        path_folder = self.folder_field.get()
+        with open('recent_comp_paths.txt', 'w') as f:
+            f.write(path_folder + '\n')
+        OpenPathImage(self.update_a, path_folder)
     
     def submit_drive(self):
-        OpenPathImage(self.update_a, self.drives_text_field.get())
+        path_drive = self.drives_field.get()
+        with open('recent_comp_paths.txt', 'w') as f:
+            f.write(path_drive + '\n')
+        OpenPathImage(self.update_a, path_drive)
+    
+    def submit_recent(self):
+        recent_idx = self.recent_field.curselection()[0]
+        OpenPathImage(self.update_a, self.recent_ls[recent_idx])
+
 
 
 class OpenPathImage:
@@ -401,31 +445,31 @@ class OpenPathImage:
         #     self.im_files = proc.map(self.find_images(avaible_hard_drives))
         
         # print(self.update_b)
-        imgs_path = r'D:\My Designs\1A Games\ZombiePunisherData\ZombiePunisher_zombieShooterProject\zombieEnvirontments blend files\lavaVolcanoLevel'
+        imgs_path = r'D:\frankenstein_s escape\images'
         if '\\' in self.update_b:
             self.im_files = self.find_images([self.update_b])  # avaible_hard_drives ['C:\\']['D:\\']
         else:
             res=[f'{x.upper()}:\\' for x in self.update_b.split(',')]
-            print(res)
+            # print(res)
             self.im_files = self.find_images(res)
         for x in self.im_files:
-            self.imbox.insert(tk.END, x)        
-
-    def select_images(self):
-        pass
+            self.imbox.insert(tk.END, x.split('\\')[-1])
     
     def find_images(self, im_drives):
         f_im = []
         for drive in im_drives:
             for r, d, files in os.walk(drive):
                 for fl in files:
-                    if fl.endswith('.jpg'):  #  or fl.endswith('.png')
+                    if fl.endswith('.jpg') or fl.endswith('.png'):  # 
                         f_im.append(f'{r}\\\{fl}')
         return f_im
     
     def submit(self):
         img_idx = self.imbox.curselection()[0]
-        self.update_a(self.im_files[img_idx])
+        path_a = self.im_files[img_idx]
+        with open('recent_comp_paths.txt', 'w') as f:
+            f.write(path_a + '\n')
+        self.update_a(path_a)
 
 
 class OpenUrlImage:
@@ -458,7 +502,10 @@ class OpenUrlImage:
         button.place(x=10, y=60, width=100, height=30)
 
     def submit(self):
-        self.update(self.textfield.get())
+        path_a = self.textfield.get()
+        with open('recent_url_paths.txt', 'w') as f:
+            f.write(path_a + '\n')
+        self.update(path_a)
 
 # class SaveAs:
 #     def __init__(self, update):
