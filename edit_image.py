a08c817adaff0431677f6ca0b22ad042d2518f89
PiotrekGelert <piotrekgelert@gmail.com>
2023-11-13 17:36:15 +0000

    small maintenance of edit_image.py

--- edit_image.py
+++ edit_image.py
@@ -6,38 +6,38 @@ from PIL import ImageTk
 class CropImage:
     def __init__(self, update, img):
         top = tk.Toplevel()
-        top.title()
-        top.geometry('400x400')
+        top.title('Crop image')
+        top.geometry('400x100')
         self.update = update
         self.image = img
 
-        left_txt = tk.Label(top,text='Old left edge to the new left:')
-        left_txt.pack()
-        left_txt.place(x=10, y=50)  # 30 50
-        self.left_input = tk.Entry(top)
-        self.left_input.pack()
-        self.left_input.place(x=10, y=80, width=150, height=20)  # 50 80
+        # left_txt = tk.Label(top,text='Old left edge to the new left:')
+        # left_txt.pack()
+        # left_txt.place(x=10, y=50)  # 30 50
+        # self.left_input = tk.Entry(top)
+        # self.left_input.pack()
+        # self.left_input.place(x=10, y=80, width=150, height=20)  # 50 80
 
-        upper_txt = tk.Label(top,text='Old top edge to the new top:')
-        upper_txt.pack()
-        upper_txt.place(x=180, y=50)  # 30 50
-        self.upper_input = tk.Entry(top)
-        self.upper_input.pack()
-        self.upper_input.place(x=180, y=80, width=150, height=20)  # 50 80
+        # upper_txt = tk.Label(top,text='Old top edge to the new top:')
+        # upper_txt.pack()
+        # upper_txt.place(x=180, y=50)  # 30 50
+        # self.upper_input = tk.Entry(top)
+        # self.upper_input.pack()
+        # self.upper_input.place(x=180, y=80, width=150, height=20)  # 50 80
 
-        right_txt = tk.Label(top,text='Old left edge to the new right:')
-        right_txt.pack()
-        right_txt.place(x=10, y=100)  # 80 100
-        self.right_input = tk.Entry(top)
-        self.right_input.pack()
-        self.right_input.place(x=10, y=120, width=150, height=20)  # 100 120
+        # right_txt = tk.Label(top,text='Old left edge to the new right:')
+        # right_txt.pack()
+        # right_txt.place(x=10, y=100)  # 80 100
+        # self.right_input = tk.Entry(top)
+        # self.right_input.pack()
+        # self.right_input.place(x=10, y=120, width=150, height=20)  # 100 120
 
-        lower_txt = tk.Label(top,text='Old top edge to the new bottom:')
-        lower_txt.pack()
-        lower_txt.place(x=180, y=100)  # 80 100
-        self.lower_input = tk.Entry(top)
-        self.lower_input.pack()
-        self.lower_input.place(x=180, y=120, width=150, height=20)  # 100 120
+        # lower_txt = tk.Label(top,text='Old top edge to the new bottom:')
+        # lower_txt.pack()
+        # lower_txt.place(x=180, y=100)  # 80 100
+        # self.lower_input = tk.Entry(top)
+        # self.lower_input.pack()
+        # self.lower_input.place(x=180, y=120, width=150, height=20)  # 100 120
 
         get_dims_button = tk.Button(
             top,
@@ -46,35 +46,35 @@ class CropImage:
         get_dims_button.pack()
         get_dims_button.place(x=10, y=10, width=380, height=30)
         
-        apply_button = tk.Button(
-            top, text='Apply changes',
-            command=self.submit_apply
-        )
-        apply_button.pack()
-        apply_button.place(x=10, y=150, width=100)  # 130 150
+        # apply_button = tk.Button(
+        #     top, text='Apply changes',
+        #     command=self.submit_apply
+        # )
+        # apply_button.pack()
+        # apply_button.place(x=10, y=150, width=100)  # 130 150
         cancel_button = tk.Button(
             top, text= 'Cancel',
             command=lambda: [self.submit_cancel(), top.destroy()]
         )
         cancel_button.pack()
-        cancel_button.place(x=130, y=150, width=100)  # 130 150
+        cancel_button.place(x=50, y=50, width=100, height=30)  # 130 150
         exit_button = tk.Button(
-            top, text='Exit',
+            top, text='Apply changes & Exit',
             command=lambda:[self.submit_exit(), top.destroy()]
         )
         exit_button.pack()
-        exit_button.place(x=260, y=150, width=100)  # 130 150
+        exit_button.place(x=170, y=50, width=190, height=30)  # 130 150
 
-    def submit_apply(self):
-        cancel=False
-        new_dimensions = (
-            cancel,
-            int(self.left_input.get()),
-            int(self.upper_input.get()),
-            int(self.right_input.get()),
-            int(self.lower_input.get())
-            )
-        self.update(new_dimensions)
+    # def submit_apply(self):
+    #     cancel=False
+    #     new_dimensions = (
+    #         cancel,
+    #         int(self.left_input.get()),
+    #         int(self.upper_input.get()),
+    #         int(self.right_input.get()),
+    #         int(self.lower_input.get())
+    #         )
+    #     self.update(new_dimensions)
     
     def submit_cancel(self):
         cancel=True
@@ -162,7 +162,9 @@ class DimensionsImage:
         
         self.canvas.bind('<Button-1>', self.gather_coords)
         self.canvas.bind('<B1-Motion>', self.update_coords)
-        self.canvas.bind('<Button-3>', lambda event: [self.submit_coords(), top.destroy()])
+        self.canvas.bind('<Button-3>', lambda event: [
+                                                        self.submit_coords(),
+                                                        top.destroy()])
 
         top.mainloop()
     
